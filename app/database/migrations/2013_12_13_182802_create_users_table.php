6a0df7a0edd716b81f5c46f303899735d34d48ba
Samet ATABAŞ <sametatabasch@gmail.com>
2014-11-01 17:14:17 +0200

    users table birthday format update

--- app/database/migrations/2013_12_13_182802_create_users_table.php
+++ app/database/migrations/2013_12_13_182802_create_users_table.php
@@ -18,7 +18,7 @@ class CreateUsersTable extends Migration {
 			$table->string('password');
 			$table->string('name');
 			$table->string('lastName');
-			$table->date('birthday')->default('00.00.000');
+			$table->date('birthday')->default('0000.00.00');
 			$table->string('role');
 			$table->timestamps();
 			$table->softDeletes();
