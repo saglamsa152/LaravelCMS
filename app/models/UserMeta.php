15c519514a52949d844ce4f786377addc0697668
Samet ATABAŞ <sametatabasch@gmail.com>
2014-11-04 19:46:44 +0200

    değeri 0 olan veriler olduğunda o veriyi sürekli yeniden ekleme sorununu çözdüm

--- app/models/UserMeta.php
+++ app/models/UserMeta.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by PhpStorm.
  * User: sametatabasch
@@ -15,17 +16,17 @@ class UserMeta extends Eloquent {
 	protected $guarded = array( 'id', 'created_at', 'updated_at' );
 
 	public function user() {
-		return $this->belongsTo( 'User','id' );
+		return $this->belongsTo( 'User', 'id' );
 	}
 
 	/**
-	 * @param $id meta bilgisi alınacak  kullanıcı id
+	 * @param $id  meta bilgisi alınacak  kullanıcı id
 	 * @param $key istenilen meta key
 	 *
 	 * @return bool|mixed
 	 */
-	public	function getMeta($id,$key){
-		$meta=$this->where('metaKey','=',$key)->where('userId','=',$id)->pluck('metaValue');
+	public function getMeta( $id, $key ) {
+		$meta = $this->where( 'metaKey', '=', $key )->where( 'userId', '=', $id )->pluck( 'metaValue' );
 		return is_null( $meta ) ? false : $meta;
 	}
 
@@ -34,11 +35,12 @@ class UserMeta extends Eloquent {
 	 * @param $key
 	 * @param $value
 	 */
-	public function setMeta($id,$key,$value){
-		if($this->getMeta($id,$key)){
-			$this->where('userId','=',$id)->where('metaKey','=',$key)->update(array('metaValue'=>$value));
-		}else{
-			$this->create(array('userId'=>$id,'metaKey'=>$key,'metaValue'=>$value));
+	public function setMeta( $id, $key, $value ) {
+		if ( false !== $this->getMeta( $id, $key ) ) {
+			$this->where( 'userId', '=', $id )->where( 'metaKey', '=', $key )->update( array( 'metaValue' => $value ) );
+		}
+		else {
+			$this->create( array( 'userId' => $id, 'metaKey' => $key, 'metaValue' => $value ) );
 		}
 	}
 
