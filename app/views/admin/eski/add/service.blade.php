b2092a075c8eeb8c138da79f91682c48b37e25eb
Samet ATABAŞ <sametatabasch@gmail.com>
2014-11-14 19:59:43 +0200

    deleted old admin template

--- app/views/admin/eski/add/service.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sametatabasch
- * Date: 28.02.2014
- * Time: 00:07
- */ 
--- app/views/admin/eski/options.blade.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sametatabasch
- * Date: 28.02.2014
- * Time: 01:13
- */ 
