f2f5d4b318c056f63026a80bc445ab5ac1a89df7
Samet ATABAŞ <sametatabasch@gmail.com>
2014-11-15 16:31:47 +0200

    getMeta ve setMeta metodları eklendi

--- app/models/PostMeta.php
+++ app/models/PostMeta.php
@@ -16,7 +16,33 @@ class PostMeta extends Eloquent {
 	protected $guarded = array( 'id', 'created_at', 'updated_at' );
 
 	public function post() {
-		return $this->belongsTo( 'Post','id' );
+		return $this->belongsTo( 'Post', 'id' );
 	}
 
+	/**
+	 * @param $id  meta bilgisi alınacak  kullanıcı id
+	 * @param $key istenilen meta key
+	 *
+	 * @return bool|mixed
+	 */
+	public function getMeta( $id, $key, $unserialize = false ) {
+		$meta = $this->where( 'metaKey', '=', $key )->where( 'postId', '=', $id )->pluck( 'metaValue' );
+		if ( $unserialize ) $meta = unserialize( $meta );
+		return is_null( $meta ) ? false : $meta;
+	}
+
+	/**
+	 * @param $id meta bilgisi  eklenecek  kullanıcının id numarası
+	 * @param $key
+	 * @param $value
+	 */
+	public function setMeta( $id, $key, $value,$serialize = false  ) {
+		if ( $serialize ) $value = serialize( $value );
+		if ( false !== $this->getMeta( $id, $key ) ) {
+			$this->where( 'postId', '=', $id )->where( 'metaKey', '=', $key )->update( array( 'metaValue' => $value ) );
+		}
+		else {
+			$this->create( array( 'postId' => $id, 'metaKey' => $key, 'metaValue' => $value ) );
+		}
+	}
 }
