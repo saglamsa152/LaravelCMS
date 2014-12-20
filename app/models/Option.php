1730371adbe75b6360202a74e27cbb544e9be9b9
Samet ATABAŞ <sametatabasch@gmail.com>
2014-12-20 19:10:22 +0200

    month metodu eklendi

--- app/models/Option.php
+++ app/models/Option.php
@@ -51,4 +51,11 @@ class Option extends Eloquent {
 		}
 	}
 
+	/**
+	 * Yılın aylarını dizi olarak döner
+	 * @return array
+	 */
+	public static function months() {
+		return array( '01' => _( 'January' ), '02' => _( 'February' ), '03' => _( 'March' ), '04' => _( 'April' ), '05' => _( 'May' ), '06' => _( 'June' ), '07' => _( 'July ' ), '08' => _( 'August' ), '09' => _( 'September' ), '10' => _( 'October' ), '11' => _( 'November' ), '12' => _( 'December' ) );
+	}
 }
