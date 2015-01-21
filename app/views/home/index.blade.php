6049e6d88219010af001ebedafda10bf00bdf622
Samet ATABAŞ <sametatabasch@gmail.com>
2015-01-22 01:30:09 +0200

    ana sayfa düzenlendi. dil dosyaları güncellendi

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -20,72 +20,22 @@
 		<!-- content -->
 		<article id="content">
 			<div class="wrapper row">
-				<section class="col-1-4">
-					<div class="wrap-col">
-						<h3>
-							<span class="dropcap">A</span>
-							Business
-							<span>planning</span>
-						</h3>
-						<p class="pad_bot1">Progress is one of
-							<a target="_blank" href="http://blog.templatemonster.com/free-website-templates/">free website templates</a> created by TemplateMonster.com, optimized for 1024x768 res.
-						</p>
-						<a class="link1" href="#">Read More</a>
-					</div>
-				</section>
-				<section class="col-1-4">
-					<div class="wrap-col">
-						<h3>
-							<span class="dropcap">B</span>
-							Business
-							<span>strategies</span>
-						</h3>
-						<p class="pad_bot1">This
-							<a href="http://blog.templatemonster.com/2011/07/11/free-website-template-slider-typography/">Progress Template</a> goes with two packages &ndash; with PSD source files and without them.
-						</p>
-						<a class="link1" href="#">Read More</a>
-					</div>
-				</section>
-				<section class="col-1-4">
-					<div class="wrap-col">
-						<h3>
-							<span class="dropcap">C</span>
-							Powerful
-							<span>analytics</span>
-						</h3>
-						<p class="pad_bot1">PSD source files are available for free for registered members. The basic package is available for anyone.</p>
-						<a class="link1" href="#">Read More</a>
-					</div>
-				</section>
-				<section class="col-1-4">
-					<div class="wrap-col">
-						<h3>
-							<span class="dropcap">D</span>
-							Worldwide
-							<span>solutions</span>
-						</h3>
-						<p class="pad_bot1">This website template has several pages: Home, News, Services, Products, Contacts (contact form doesn’t work).</p>
-						<a class="link1" href="#">Read More</a>
-					</div>
-				</section>
-			</div>
-			<div class="wrapper row">
 				<section class="col-3-4">
 					<div class="wrap-col">
-						<h2 class="under">Welcome,dear visitor!</h2>
+						<h2 class="under">Hoşgeldiniz!</h2>
 						<div class="wrapper">
 							<figure class="left marg_right1"><img alt="" src="{{URL::asset('assets/images/page1_img1.jpg')}}">
 							</figure>
-							<p class="pad_bot1">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa.</p>
+							<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam scelerisque porttitor nibh ac laoreet. Maecenas molestie magna ut quam lacinia finibus. In non magna eu ex efficitur accumsan efficitur non massa. Pellentesque eu est a tellus tristique suscipit ac fermentum eros. Suspendisse potenti. Fusce condimentum rutrum nulla, a feugiat dolor imperdiet nec. Aenean purus ligula, bibendum tincidunt placerat id, blandit id turpis. Maecenas varius vulputate ante, et lobortis felis volutpat at. Interdum et malesuada fames ac ante ipsum primis in faucibus.</p>
 
-							<p>
-								Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus.</p>
+							<p>Nulla nec tincidunt leo. Aliquam arcu libero, ornare sed dignissim nec, dictum a lorem. Vivamus tristique leo efficitur aliquet rhoncus. Aliquam malesuada vulputate leo eu porta. Mauris sodales ornare tristique. Aenean vulputate risus ut ex convallis ornare. Nulla aliquet nisl at lacus dapibus, sit amet posuere purus sagittis. Donec in dolor tellus. Aliquam a consectetur nisl. Donec dapibus eu ipsum ac consectetur. Vivamus quis erat a elit pellentesque sodales. Phasellus laoreet, nisl nec luctus auctor, est risus cursus ligula, et hendrerit tortor ligula non tortor.</p>
+
 						</div>
 					</div>
 				</section>
 				<section class="col-1-4">
 					<div class="wrap-col">
-						<h2>Testimonials</h2>
+						<h2>Duyurular</h2>
 						<div class="testimonials">
 							<div id="testimonials" style="visibility: visible; overflow: hidden; position: relative; z-index: 2; left: 0px; height: 210px;">
 								<ul style="margin: 0px; padding: 0px; position: relative; list-style-type: none; z-index: 1; height: 1050px; top: -210px;">
