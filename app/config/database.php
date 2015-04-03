5ee17734d962ba24cf038e9c8600215750735a7e
Samet ATABAŞ <sametatabasch@gmail.com>
2015-04-03 21:32:37 +0300

    gitignore update, database name update

--- app/config/database.php
+++ app/config/database.php
@@ -55,7 +55,7 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'dernek',
+			'database'  => 'laravel_dernek',
 			'username'  => 'laravel',
 			'password'  => '123456',
 			'charset'   => 'utf8',
