e9e31bda70e1741e3d742910439753989d040b10
Samet ATABAŞ <sametatabasch@gmail.com>
2015-06-27 02:17:01 +0300

    Laravel güncellemesi için tüm dosyaları siliyorum

--- app/views/admin/rightSide/list/orders.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sametatabasch
- * Date: 01.05.2014
- * Time: 17:26
- */ 
--- app/views/admin/rightSide/mail/settings.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: sametatabasch
- * Date: 03.04.2015
- * Time: 22:49
- */
