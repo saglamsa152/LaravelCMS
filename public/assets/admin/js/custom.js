d2b2d3b2ee115890191d36134d5946ae5466e183
sametatabasch <sametatabasch@gmail.com>
2015-09-19 14:17:49 +0300

    datatable türkçe desteği eklendi

--- public/assets/admin/js/custom.js
+++ public/assets/admin/js/custom.js
@@ -7,7 +7,26 @@ $(function () {
 	 * dataTable sınıflı  tablolar DataTeble ile düzenleniyor
 	 */
 	if ($.isFunction($.fn.dataTable)) {
-		$('.dataTable').dataTable();
+		$('.dataTable').dataTable( {
+			"language": {
+				"sProcessing":   "İşleniyor...",
+				"sLengthMenu":   "Sayfada _MENU_ Kayıt Göster",
+				"sZeroRecords":  "Eşleşen Kayıt Bulunmadı",
+				"sInfo":         "  _TOTAL_ Kayıttan _START_ - _END_ Arası Kayıtlar",
+				"sInfoEmpty":    "Kayıt Yok",
+				"sInfoFiltered": "( _MAX_ Kayıt İçerisinden Bulunan)",
+				"sInfoPostFix":  "",
+				"search":       "Bul:",
+				"sUrl":          "",
+				"oPaginate": {
+					"sFirst":    "İlk",
+					"sPrevious": "Önceki",
+					"sNext":     "Sonraki",
+					"sLast":     "Son"
+				}
+
+			}
+		} );
 	}
 	/**
 	 *  data-mask özelliği olan nesneye input mask uygular
