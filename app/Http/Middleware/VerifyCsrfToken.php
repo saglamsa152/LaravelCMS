939f04dae8fa78b71e87075b07f9ed7570a0d12c
Samet ATABAŞ <sametatabasch@gmail.com>
2015-12-24 13:54:59 +0200

    #85 Tooken hatası yakalanarak uyarı verilmesi sağlandı. fakat süre uzatılmalı

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -2,6 +2,7 @@
 
 use Closure;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken as BaseVerifier;
+use Illuminate\Session\TokenMismatchException;
 
 class VerifyCsrfToken extends BaseVerifier {
 
@@ -14,7 +15,16 @@ class VerifyCsrfToken extends BaseVerifier {
 	 */
 	public function handle($request, Closure $next)
 	{
-		return parent::handle($request, $next);
+		try {
+			return parent::handle($request, $next);
+		} catch(TokenMismatchException $e){
+			if ($request->ajax())
+			{
+				//return response('Unauthorized.', 401);
+				$ajaxResponse = array('status' => 'danger', 'msg' => 'Authenticate time out, Please login again');
+				return response()->json($ajaxResponse);
+			}
+		}
 	}
 
 }
